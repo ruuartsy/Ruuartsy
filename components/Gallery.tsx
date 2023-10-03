e77e8100e7d5ba3d279afc292f1e0d34eafe1fd1
ruuartsy <146751470+ruuartsy@users.noreply.github.com>
2023-10-03 14:29:21 +0530

    Update Gallery.tsx

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -10,44 +10,23 @@ export const Gallery = () => {
     {
       name: 'Desk and Office',
       description: 'Work from home accessories',
-      imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-01.jpg',
+      imageSrc: 'https://scontent.fcmb3-2.fna.fbcdn.net/v/t39.30808-6/384989326_122120435162031337_1477327784974440547_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=49d041&_nc_ohc=GgChyRskMawAX8Nmr0r&_nc_zt=23&_nc_ht=scontent.fcmb3-2.fna&oh=00_AfCQYQY9OTZj2hfXjPkLkJGqd00MVRoXkMDBX2_W1aDm9A&oe=652180BA',
       imageAlt: 'Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug.',
-      href: '#',
+      href: '',
     },
     {
       name: 'Self-Improvement',
       description: 'Journals and note-taking',
-      imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-02.jpg',
+      imageSrc: 'https://scontent.fcmb3-2.fna.fbcdn.net/v/t39.30808-6/384996553_122120423186031337_4162787377204629679_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=49d041&_nc_ohc=enPPZzgJSacAX9UQMUM&_nc_zt=23&_nc_ht=scontent.fcmb3-2.fna&oh=00_AfCGdIVSJF1-vrPDHqrfCd5iMbObyq2m0P3Jsn29pK3qiw&oe=651FE406',
       imageAlt: 'Wood table with porcelain mug, leather journal, brass pen, leather key ring, and a houseplant.',
       href: '#',
     },
     {
       name: 'Travel',
       description: 'Daily commute essentials',
-      imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-03.jpg',
+      imageSrc: 'https://scontent.fcmb3-2.fna.fbcdn.net/v/t39.30808-6/384989326_122120435162031337_1477327784974440547_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=49d041&_nc_ohc=GgChyRskMawAX8Nmr0r&_nc_zt=23&_nc_ht=scontent.fcmb3-2.fna&oh=00_AfCQYQY9OTZj2hfXjPkLkJGqd00MVRoXkMDBX2_W1aDm9A&oe=652180BA',
       imageAlt: 'Collection of four insulated travel bottles on wooden shelf.',
       href: '#',
-    },
-    {
-      name: 'Desk and Office',
-      description: 'Work from home accessories',
-      imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-01.jpg',
-      imageAlt: 'Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug.',
-      href: '#',
-    },
-    {
-      name: 'Self-Improvement',
-      description: 'Journals and note-taking',
-      imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-02.jpg',
-      imageAlt: 'Wood table with porcelain mug, leather journal, brass pen, leather key ring, and a houseplant.',
-      href: '#',
-    },
-    {
-      name: 'Travel',
-      description: 'Daily commute essentials',
-      imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-03.jpg',
-      imageAlt: 'Collection of four insulated travel bottles on wooden shelf.',
-      href: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-03.jpg',
     },
   ]
   
